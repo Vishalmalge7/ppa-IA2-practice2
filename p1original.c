404e0374cb41538812e8d7b5af1301e22999b17d
VISHAL <97095904+Vishalmalge7@users.noreply.github.com>
2022-02-09 04:35:59 +0000

    one function changed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,13 @@
+#include<stdio.h>  
+int main()   
+{ 
+  float b , h, area;
+  b = 1;
+  h = 2;
 
-}
+  area = (b*h) / 2 ;  
+  
+  printf("\n\n Area of Triangle is: %f",area);  
+  return (0);  
+  
+}  
